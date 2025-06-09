721944029242efa5f02f7269aca50bc3879e4d4d
Hyeon woo <renzpark@naver.com>
2025-06-09 16:59:20 +0900

    Lv1 완성

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    @NotBlank
+    @NotBlank(message = "기존 비밀번호는 필수입니다.")
     private String oldPassword;
-    @NotBlank
+
+    @NotBlank(message = "새 비밀번호는 필수입니다.")
+    @Size(min = 8, message = "새 비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(regexp = ".*\\d.*", message = "새 비밀번호에는 숫자가 포함되어야 합니다.")
+    @Pattern(regexp = ".*[A-Z].*", message = "새 비밀번호에는 대문자가 포함되어야 합니다.")
     private String newPassword;
 }
